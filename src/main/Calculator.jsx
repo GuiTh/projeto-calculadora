86faeaf2c72e5ace01cced85458550a9d7f70089
GuiTh <guilhermetcarlos@hotmail.com>
2022-09-16 13:17:07 -0300

    att calc

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import './Calculator.css'
+
+/*exportando o componente calculadora
+e criando a classe render para renderizar a calculadora (a classe render precisa de um return no fim)
+*/
+export default class Calculator extends Component{
+    render(){
+        return(
+/*isso nao é html, é jsx que sera transpilado para js puro */
+    <div className="calculator">
+
+    </div>
+        )
+    }
+}
